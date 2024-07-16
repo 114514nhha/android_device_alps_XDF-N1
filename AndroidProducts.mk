b362e77a29dbe1b72907852efe0a4cafc233cb3b
114514nhha <shang_114514@qq.com>
2024-07-16 13:46:26 +0800

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_XDF-N1.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_XDF-N1-user \
+    omni_XDF-N1-userdebug \
+    omni_XDF-N1-eng
---
+++ omni_XDF-N1.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from XDF-N1 device
+$(call inherit-product, device/alps/XDF-N1/device.mk)
+
+PRODUCT_DEVICE := XDF-N1
+PRODUCT_NAME := omni_XDF-N1
+PRODUCT_BRAND := XDF
+PRODUCT_MODEL := XDF-N1
+PRODUCT_MANUFACTURER := alps
+
+PRODUCT_GMS_CLIENTID_BASE := android-joyar
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_tb8788p1_64_bsp-user 10 QP1A.190711 1671764242 release-keys"
+
+BUILD_FINGERPRINT := alps/full_tb8788p1_64_bsp/tb8788p1_64_bsp:10/QP1A.190711/1671764242:user/release-keys
