4cf3e43619f4b954e92c895b6fe1e61a5122d615
114514nhha <122505515+114514nhha@users.noreply.github.com>
2024-07-17 10:52:26 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_XDF-N1-user
-add_lunch_combo omni_XDF-N1-userdebug
-add_lunch_combo omni_XDF-N1-eng
+add_lunch_combo omni_XDF_N1-user
+add_lunch_combo omni_XDF_N1-userdebug
+add_lunch_combo omni_XDF_N1-eng
